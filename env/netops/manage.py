b59715c70349544ca409936efbc94c1b5bbe4cc2
ms168085 <ms168085@fi365.ort.edu.uy>
2024-05-01 13:55:40 +0000

    Proyecto Configurado, vacio, corriendo ok

--- env/netops/manage.py
+++ env/netops/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netops.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netops.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
---
+++ env/netops/netops/settings/local.py
@@ -0,0 +1,31 @@
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["151.106.108.193", "localhost"]
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR.child('db.sqlite3'),
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
