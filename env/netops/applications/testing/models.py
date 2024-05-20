95c54b322355997fd229ef75a65d4c48a2b2d368
ms168085 <ms168085@fi365.ort.edu.uy>
2024-05-20 20:37:18 +0000

    Se corrige la relacion entre testing y usuario

--- env/netops/applications/testing/models.py
+++ env/netops/applications/testing/models.py
@@ -5,7 +5,7 @@ from ..users.models import User
 class Testing(models.Model):
     funcionalidad = models.CharField("Funcionalidad", max_length=200)
     comentario = models.TextField(blank=True, null=True)
-    usuario = models.OneToOneField(User, related_name="usuario", on_delete=models.CASCADE, blank=True, null=True)
+    usuario = models.ForeignKey(User, related_name="tareas", on_delete=models.CASCADE, blank=True, null=True)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
