a9f731cd29193a0a8763e5988ebd21bbe987db4e
ms168085 <ms168085@fi365.ort.edu.uy>
2024-06-22 22:14:15 +0000

    Implementacion de la vista y url para registrar la VPN

--- env/netops/applications/vpn/forms.py
+++ env/netops/applications/vpn/forms.py
@@ -4,4 +4,36 @@ from .models import Vpn
 class RegistrarVPNForm(forms.ModelForm):
     class Meta:
         model = Vpn
-        fields = "__all__"
+        fields = fields = [
+            'con_sitio_central',
+            'vpn_instance',
+            'tinco_movil',
+            'route_distinguisher',
+            'vprn_id',
+            'ip_mag',
+            'peer_mag',
+            'ip_mun',
+            'peer_mun',
+            'conectividad_entre_moviles',
+            'nombre_pool',
+            'server_ip',
+            'wildcard',
+            'acl_node',
+            'nombre_acl',
+            'prioridad',
+            'nombre_pool_moviles',
+            'primer_ip_mag',
+            'ultima_ip_mag',
+            'primer_ip_mun',
+            'ultima_ip_mun',
+            'roaming',
+            'apn',
+            'dns_1',
+            'dns_2',
+            'cliente_acm',
+            'apn_id',
+            'tinco_fija',
+            'empresa',
+            'ip_router',
+            'contacto'
+        ]
--- env/netops/applications/vpn/views.py
+++ env/netops/applications/vpn/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import FormView
 from .forms import RegistrarVPNForm
+from django.urls import reverse_lazy
+from django.contrib import messages
+from .models import Vpn
 
 # Create your views here.
-class RegistrarVPN(FormView):
+class RegistrarVPN(LoginRequiredMixin, FormView):
     template_name = 'vpn/registrar.html'
     form_class = RegistrarVPNForm
-    success_url = '/'
+    success_url = reverse_lazy('users_app:index')
+
+    def form_valid(self, form):
+        vpn = form.save(commit=False)
+        vpn.usuario = self.request.user
+        vpn.save()
+        messages.success(self.request, 'La VPN ha sido creada con éxito.')
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, 'Error al crear la VPN. Por favor, revise los datos ingresados.')
+        return self.render_to_response(self.get_context_data(form=form))
