19d30e84844ae60b99eef77dca5777b236aaa82c
ms168085 <ms168085@fi365.ort.edu.uy>
2024-08-04 05:14:45 +0000

    Se crea funcionalidad de listado de tareas

--- env/netops/applications/tareas/urls.py
+++ env/netops/applications/tareas/urls.py
@@ -6,4 +6,6 @@ app_name = 'tareas_app'
 urlpatterns = [
     path('registrar_tarea/', views.TareaCreateView.as_view(), name='registrar_tarea'),
     path('listar_tareas/', views.TareasListView.as_view(), name='listar_tareas'),
+    path('informe_de_tareas/', views.InformeDeTareas.as_view(), name='informe_de_tareas'),
+    path('descargar_informe/', views.descargar_informe, name='descargar_informe'),
 ]
--- env/netops/applications/tareas/views.py
+++ env/netops/applications/tareas/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Tarea
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, TemplateView
 
 # Create your views here.
 
@@ -33,7 +34,34 @@ class TareasListView(LoginRequiredMixin, ListView):
     template_name = 'tareas/listar_tareas.html'
     paginate_by = 10
     login_url = 'users_app:user_login'
+    context_object_name = 'tareas'
 
     def get_queryset(self):
         tareas = Tarea.objects.all().order_by('-created')
         return tareas
+
+class InformeDeTareas(LoginRequiredMixin, TemplateView):
+    template_name = 'tareas/informe_de_tareas.html'
+    login_url = 'users_app:user_login'
+
+# FUNCIÓN PARA DESCARGAR EL INFORME EN FORMATO EXCEL
+def descargar_informe(request):
+    if request.method == 'POST':
+        fecha_desde = request.POST.get('fecha_desde')
+        fecha_hasta = request.POST.get('fecha_hasta')
+        tareas = Tarea.objects.filter(fecha__range=[fecha_desde, fecha_hasta])
+
+        # Lógica para generar el informe
+        # Por ejemplo, podrías generar un archivo CSV, PDF, etc.
+        # Aquí simplemente devolvemos una respuesta de texto plano como ejemplo
+        informe = "Informe de Tareas\n"
+        for tarea in tareas:
+            informe += f"{tarea.fecha} - {tarea.descripcion}\n"
+
+        # Crear una respuesta HTTP con el contenido del informe
+        response = HttpResponse(informe, content_type='text/plain')
+        response['Content-Disposition'] = 'attachment; filename="informe_tareas.txt"'
+        return response
+
+    # Si el metodo no es POST
+    return render(request, 'tareas_app/informe_de_tareas.html')
